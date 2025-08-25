ac14f6d29d786cffc082ec33d748a99f910cacf0
inmove <lisper.inmove@gmail.com>
2025-08-25 22:08:13 +0800

    Add usage of builder

--- include/builder.h
+++ include/builder.h
@@ -116,3 +116,22 @@ public:
 private:
 	ICarBuilder* builder_ = nullptr;
 };
+
+/**
+#include <vector>
+#include <iostream>
+#include "builder.h"
+
+using namespace std;
+
+int main()
+{
+	Director director;
+	SportsCarBuilder sportsBuilder;
+	director.setBuilder(&sportsBuilder);
+	director.makeSportsCar();
+	Car car = sportsBuilder.getResult();
+	car.list();
+	return 0;
+}
+*/
